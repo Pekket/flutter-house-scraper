15c387a244c7ddd8a1eeaf24874701d6d786ba15
Jan Pecquet <jan.pecquet@outlook.com>
2020-01-02 14:55:22 +0100

    Updated icons

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kHouseUrl = "http://87808ca3.ngrok.io/api/house?providers=";
+const kHouseUrl = "";
 const kDefaultHomeImageUrl = "http://www.propertyindetroit.com/wp-content/uploads/2018/09/default-property-propertyindetroit.jpg";
 
 var kHouseProviders = ["zimmo", "immoscoop", "immoweb", "immovlan", "tweedehands"];
